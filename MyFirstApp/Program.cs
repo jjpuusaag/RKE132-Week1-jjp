eed0f4d42be0555a3ec9cde2e8218fcdcce6f7ab
jjpuusaag <jjuripuusaag@gmail.com>
2024-02-12 18:23:55 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,12 @@
+﻿// This is a comment.
+
+
+
+Console.WriteLine("Enter your name:");
+
+string userName = Console.ReadLine();
+
+Console.WriteLine("Hello" + ", " + userName + "! ")
+//string interpolation
+
+    Console.WriteLine($"Hello, {userName}!")
